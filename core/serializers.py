7cc02608c58f3b364d6cf09f33d5f91d01bfcb90
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2025-01-27 11:12:20 -0300

    [ADD] Serializer for models

---
+++ core/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Project, Unit, Customer
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class UnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Unit
+        fields = '__all__'
