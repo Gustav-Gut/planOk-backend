257cc1bbc3dd6e44070733fdfd119b2a9f0faaf6
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2025-01-27 11:27:09 -0300

    [ADD] routes for the api andpoints

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from rest_framework import routers
+from .views import ProjectViewSet, UnitViewSet, CustomerViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/projects', ProjectViewSet, 'projects')
+router.register('api/units', UnitViewSet, 'units')
+router.register('api/customers', CustomerViewSet, 'customers')
+
+
+urlpatterns = router.urls
